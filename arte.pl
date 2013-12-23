92424f9a413a051d7b77d5db8c52615d093e1d39
markus <markus@localhost.localdomain>
2013-12-23 18:58:36 +0100

    nun funktionfähig dateicheck fehlt noch

--- arte.pl
+++ arte.pl
@@ -4,13 +4,19 @@
 # rtmpdump -v -r "rtmp://artestras.fc.llnwd.net/artestras/s_artestras_scst_geoFRDE_de?s=1320220800&h=878865258ebb8eaa437b99c3c7598998" -o test.mp4 
 # better ! no lags ffmpeg -map 0.6 -map 0.7 -i "http://delive.artestras.cshls.lldns.net/artestras/contrib/delive.m3u8" -strict experimental test.mp4
 
+use strict;
+use warnings;
+
 my $file;
 my $old;
 my $length;
 my $stream;
 my $geturl;
+my $ID=0;
 # url from arte !
 my $url="http://arte.tv/papi/tvguide/videos/livestream/player/D/";
+my $maps="-map 0.6 -map 0.7"; #low quali
+#my $maps="-map 0.0 -map 0.1"; #high quali
 # get init url
 my $text;
 # parse the url
@@ -18,14 +24,24 @@ my $text;
 
 while (1) 
 {
+	if ($file ne $old)
+	{
+		print "Next File: $file || $stream || $ID \n";
+		$ID++;
+		echo "ffmpeg $maps -i $stream -strict experimental $file" > /tmp/run.sh
+		`screen -dmS $ID-ffmpeg sh /tmp/run.sh`;
+		getting the new pid
+		$pid = `screen -ls | grep $ID-ffmpeg | sed 's/\s+//;s/.$ID-ffmpeg.*//'`;
+		sleep 10;
+		if ( $oldpid ) 
+		{
+			`kill $oldpid`;
+		}
+		$oldpid = $pid;
+		$old = $file;
+	}
 	sleep 3;
 	&urlparse();
-	while ($file ne $old)
-	{
-		print "rtmpdump -v -r \"$stream$geturl\" -m $length -o $file";
-		$old = $file;
-		exit;
-	}
 }
 
 
@@ -33,22 +49,19 @@ sub urlparse()
 {
 	$text=`wget $url -qO-`;
 	$text =~ s/ /_/g;
-
+	
 	$file = $text;
 	$file =~ s/.*"VTI":"//;
 	$file =~ s/".*/.mp4/;
 	
-    $length = $text;
-    $length =~ s/.*"VTI":.*"BDS"://;
-    $length =~ s/,".*//;
-
 	$stream = $text;
-	$stream =~ s/.*"streamer":"//;
-	$stream =~ s/".*//;
+	$stream =~ s/.*"bitrate":.*"url":"http/http/;
+    $stream =~ s/".*//;	
 	
-	$geturl = $text;
-	$geturl =~ s/.*"streamer":.*"url":"s_/s_/;
-    $geturl =~ s/".*//;	
-
-	print "Next File: $file || $length secs || $stream || $geturl\n";
+	if (!$file || !$stream)
+	{
+		$file = `date +"%Y%m%d_%H%M.mp4"`;
+		$stream = "http://delive.artestras.cshls.lldns.net/artestras/contrib/delive.m3u8";
+	}
+	print "Get: $file || $stream \n";
 }
