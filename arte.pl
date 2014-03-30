36eeb4b12d80ce7d77c743b1684e8be7d0dc0fd9
markus <markus@localhost.localdomain>
2014-03-30 23:24:54 +0200

    Exclude liste for Magazine and files with same name

--- arte.pl
+++ arte.pl
@@ -9,12 +9,14 @@ use warnings;
 
 
 my $file; # filename to write 
-my $ogfolder="/home/markus/arte/stream"; # folder to store
+my $ogfolder="~/arte/stream"; # folder to store
+my $exclude="exclude"; # exclude list from doublicate checking
 my $old=" "; # old filename to fetch the change of the mega data 
-my $stream; # stream m3u8 url
+#my $stream; # stream m3u8 url # old
 my $pid;
 my $oldpid=0; #very ugly !
 my $meta;
+my $json;
 my $date;
 my $ID=0; # ID to seperate the screens and kill them
 # url from arte !
@@ -23,37 +25,31 @@ my $maps="-map 0.2 -map 0.3"; #low quali
 #my $maps="-map 0.0 -map 0.1"; #high quali
 # get init url
 my $text;
+my $filename;
 
 `mkdir -p $ogfolder`;
-
-if (`ls /tmp/ID 2> /dev/null`)
-{
-   my $temp = `cat /tmp/ID`;
-   chomp($temp);
-   $ID = $temp;
-}
-print "ID ist -$ID-\n";
 
 # parse the url
 &urlparse();
 
+print "ID ist -$ID-\n";
 
 while (1) 
 {
 	if ($file ne $old)
 	{
-		print "INPUT File: $file || ID: $ID \n";
-		if ( $file =~ m/.*Live\.mp4/ ||  $file =~ m/.*ARTE_Journal\.mp4/ ) 
+		print "INPUT File: $file || ID: $ID  || orgfile: $filename\n";
+		if ( glob("$ogfolder/*$filename") && !(`grep $filename $exclude`) )
 		{
-			print "No record of live and Journal \n";
-			sleep 3;
-			&urlparse();
-			next;
+			print "e";
+            sleep 1;
+            &urlparse();
+            next;
 		}
 		#`echo "ffmpeg $maps -i $stream -strict experimental $ogfolder/$ID-$file" > /tmp/run.sh`; # writing it to a tmp sh file , because screen have problems with lots of arguement (fix me)
-		`echo "rtmpdump -v -r \\\"rtmp://artestras.fc.llnwd.net/artestras/s_artestras_scst_geoFRDE_de?s=1320220800&h=878865258ebb8eaa437b99c3c7598998\\\" -o $ogfolder/$ID-$file" > /tmp/run.sh`; 
+		`echo "rtmpdump -v -r \\\"rtmp://artestras.fc.llnwd.net/artestras/s_artestras_scst_geoFRDE_de?s=1320220800&h=878865258ebb8eaa437b99c3c7598998\\\" -o $ogfolder/$file" > /tmp/run.sh`; 
 		system("screen -dmS $ID-rtmp sh /tmp/run.sh"); # start the ffmpeg dump detached 
-		sleep 240; # wait so that the stream can start and wie capture some overlapping .
+		sleep 10; # wait so that the stream can start and wie capture some overlapping .
 		$pid = `ps aux | grep rtmp | grep "$file" | awk '{print \$2}' | head -n 1`; # get the current screen pid
 		chomp($pid);
 		print "dritter mit PID $pid\n";
@@ -68,7 +64,8 @@ while (1)
 		}
 		# write Metadata
 		chomp($meta);
-		`echo $meta > $ogfolder/$ID-$file.meta.txt`;
+		`echo $meta > $ogfolder/$file.meta.txt`;
+		`echo $json >> $ogfolder/$file.meta.txt`;
 		# debug output
 		print "runed: PID $pid , OLD $oldpid, ID $ID \n";
 		$oldpid = $pid; # for the next round 
@@ -76,7 +73,7 @@ while (1)
 		$ID++; # count the id UP
 		`echo $ID > /tmp/ID`
 	}
-	sleep 3;
+	sleep 1;
 	&urlparse();
 }
 
@@ -91,27 +88,29 @@ sub urlparse()
 	$text =~ s/\(/_/g;
 	$text =~ s/\)/_/g;
 	$text =~ s/\//_/g;
-	
+    # Arte ID
+    $ID = $text;
+    $ID =~ s/.*IID":"//;
+    $ID =~ s/".*//;
+	# dateinamen erzeugung
 	$file = $text;
 	$file =~ s/.*"VTI":"//;
 	$file =~ s/".*/.mp4/;
-	$file = "$date-$file";
-	
-	$stream = $text;
-	$stream =~ s/.*"bitrate":.*"url":"http/http/;
-    $stream =~ s/".*//;
-	
+	$filename=$file;
+	$file = "$date-$ID-$file";
+	# Json dump , debug
+	$json = $text;
+	# Meta daten 
 	$meta = $text;
 	$meta =~ s/.*"VDE":"//; 
 	$meta =~ s/",".*//;
 	$meta =~ s/_/ /g;
-	
-	if (!$file || !$stream)
+	# file backup 
+	if (!$file )
 	{
 		$date = `date +"%Y%m%d_%H"`;
 		chomp($date);
 		$file = "$date.mp4";
-		$stream = "http://delive.artestras.cshls.lldns.net/artestras/contrib/delive.m3u8";
 		print "1";
 	}
 	print ".";
