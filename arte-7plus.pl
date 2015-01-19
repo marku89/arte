442d0d56b49ceff90a7a3ce4d84ef6d3599ad644
root <root@arte.kola.li>
2015-01-19 15:17:40 +0100

    New feature , getrenntes verhalten zwischen live und 7+

---
+++ arte-7plus.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+# arte parser for 7 Plus one day 
+# IF not set to date X the past day will used and should be run at 5AM 
+# url basic http://www.arte.tv/guide/de/20141121
+
+
+use warnings;
+use strict;
+my $date;
+my $ogfolder="/arte/stream";
+my $ID;
+
+
+if ( ! $ARGV[0] )
+{
+	$date = `date -d "now -1 day" "+%Y%m%d"`;
+	chomp($date);
+}
+else
+{
+	$date=$ARGV[0];
+	chomp($date);
+}
+
+
+my @liste = `wget http://www.arte.tv/guide/de/$date -qO- | grep "<a data-track-array-click" | sed 's/<a data-track-array-click=.*href=//;s/ title.*//' | grep -v ">" | grep "[[:digit:]]" | sort | uniq`;
+
+if ( !@liste )
+{
+	print "LISTE LEER exit now \n";
+	exit;
+}
+
+foreach my $line (@liste)
+{
+	# test if guide is in the lin
+	if ( $line =~ m/guide/ )
+	{
+		$line =~ s/'//g;
+		chomp($line);
+		#print "$line    \n";
+		$ID = $line;
+		$ID =~ s/.*de\///;
+		$ID =~ s/\/.*//;
+		$ID =~ s/-/_/;
+		#print "$ID\n";
+		if ( !$ID )
+		{
+			print "ID is leer\n";
+			exit;
+		}
+		if ( ! `find $ogfolder -name "*$ID*.mp4" 2> /dev/null` )
+		{
+			print "NO $line found ... Download\n";
+			`echo "cd $ogfolder; /home/markus/arte/parsebehind.pl \\\"http://www.arte.tv$line\\\" " > /tmp/run7.sh`;
+			system("screen -dmS $ID-rtmp bash /tmp/run7.sh"); # start the ffmpeg dump detached 
+			sleep 3;
+		}
+	}		
+}
